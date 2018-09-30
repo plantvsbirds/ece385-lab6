5aa3279ebeea53d68b0bd7b149916ff229156095
pvb <plantvsbird@gmail.com>
2018-09-30 00:47:41 -0500

    make mem more like it

--- mem.sv
+++ mem.sv
@@ -1,14 +1,29 @@
 module mem (
   input logic Mem_Fetch_En, Mem_Write_En,
   inout logic Data_Bus,
+  inout logic [15:0] MAR,
+  output logic CE, UB, LB, OE, WE,
+  output logic [19:0] ADDR,
 );
   always_comb
+    assign ADDR = { 20'b0 };
+    // high-impedance value ZZZZZZ
     begin
-      if (Mem_Fetch_En) begin
-
+      if (Mem_Fetch_En || Mem_Write_En) begin
+        assign CE = 0;
+        assign UB = 0;
+        assign LB = 0;
+        assign ADDR = { 4'b0, MAR };
+        // extend to 20 bits
       end
       if (Mem_Write_En) begin
-
+        assign WE = 0;
+      end
+      if (Mem_Read_En) begin
+        assign OE = 0;
+      end
+      if (!Mem_Write_En && !Mem_Fetch_En) begin
+        assign CE = 1;
       end
     end
 endmodule // mem
