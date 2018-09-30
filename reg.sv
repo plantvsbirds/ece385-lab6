573d3b1725bdce3e5b55b50e9be8f6ffc3072df8
pvb <plantvsbird@gmail.com>
2018-09-30 00:27:45 -0500

    make reg all parallel

--- reg.sv
+++ reg.sv
@@ -1,7 +1,6 @@
 module reg_16 (
-  input  logic Clk, Reset, Shift_In, Load, Shift_En,
-  input  logic [15:0]  D,
-  output logic Shift_Out,
+  input  logic Clk, Reset, Load,
+  input  logic [15:0]  Data_In,
   output logic [15:0]  Data_Out
 );
 
@@ -10,6 +9,6 @@ module reg_16 (
  	 if (Reset)
 		  Data_Out <= 16'h0;
 	 else if (Load)
-		  Data_Out <= D;
+		  Data_Out <= Data_In;
 	 else
 endmodule
