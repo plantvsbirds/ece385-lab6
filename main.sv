eacb31b73a285e0878d3cd13bbc144e499b1c7cb
pvb <plantvsbird@gmail.com>
2018-09-30 00:50:01 -0500

    fixing main

--- main.sv
+++ main.sv
@@ -8,13 +8,16 @@ module main (
   inout logic Data_Bus, 
 );
 
+  initial begin: INIT_STATE
+    assign CE = 1;
+    assign UB = 1;
+    assign LB = 1;
+    assign OE = 1;
+    assign WE = 1;
+  end
+
   logic Mem_Fetch_En, Mem_Write_En;
 
-  control c0(.*);
-  mem m0 (
-    .Mem_Fetch_En, .Mem_Write_En,
-    .Data_Bus(Data_Bus) /* ? */
-  );
   logic [15:0] PC_In, PC_Out;
   reg PC (.Data_In(PC_In), .Data_Out(PC_Out), .*);
   logic [15:0] IR_In, IR_Out;
@@ -26,5 +29,13 @@ module main (
   logic [15:0] ST_In, ST_Out;
   reg ST (.Data_In(ST_In), .Data_Out(ST_Out), .*);
 
+  control c0(.*);
+  mem m0 (
+    .Mem_Fetch_En, .Mem_Write_En,
+    .MAR(MAR_Out),
+    .Data_Bus(Data_Bus), /* ? */
+    .CE, .UB, .LB, .OE, .WE,
+    .ADDR,
+  );
 
 endmodule // main
