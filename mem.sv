3f84ad6ad75846be451769011ccf7ce3a20d8044
hl <gfwsucksdick@protonmail.com>
2018-10-03 02:49:42 -0500

    fix mem syntax, comment out inout

--- mem.sv
+++ mem.sv
@@ -1,29 +1,34 @@
 module mem (
   input logic Mem_Fetch_En, Mem_Write_En,
-  inout logic Data_Bus,
-  inout logic [15:0] MAR,
+  // inout logic Data_Bus,
+  input logic [15:0] MAR,
   output logic CE, UB, LB, OE, WE,
-  output logic [19:0] ADDR,
+  output logic [19:0] ADDR
 );
   always_comb
-    assign ADDR = { 20'b0 };
-    // high-impedance value ZZZZZZ
     begin
+    ADDR = { 20'b0 };
+    CE = 1;
+    UB = 1;
+    LB = 1;
+    OE = 1;
+    WE = 1;
+    // high-impedance value ZZZZZZ
       if (Mem_Fetch_En || Mem_Write_En) begin
-        assign CE = 0;
-        assign UB = 0;
-        assign LB = 0;
-        assign ADDR = { 4'b0, MAR };
+        CE = 0;
+        UB = 0;
+        LB = 0;
+        ADDR = { 4'b0, MAR };
         // extend to 20 bits
       end
       if (Mem_Write_En) begin
-        assign WE = 0;
+        WE = 0;
       end
-      if (Mem_Read_En) begin
-        assign OE = 0;
+      if (Mem_Fetch_En) begin
+        OE = 0;
       end
       if (!Mem_Write_En && !Mem_Fetch_En) begin
-        assign CE = 1;
+        CE = 1;
       end
     end
 endmodule // mem
